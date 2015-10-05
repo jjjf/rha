048083ed0edf9086239c4f6c5a4fc98f1b93d860
jeff sovetts <jeff.sovetts@sap.com>
2015-10-05 14:33:48 -0400

    test

---
+++ rhatest/WebContent/rhatest/rhatest.controller.js
@@ -0,0 +1,38 @@
+sap.ui.controller("rhatest.rhatest", {
+
+/**
+* Called when a controller is instantiated and its View controls (if available) are already created.
+* Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
+* @memberOf rhatest.rhatest
+*/
+//	onInit: function() {
+//
+//	},
+
+/**
+* Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
+* (NOT before the first rendering! onInit() is used for that one!).
+* @memberOf rhatest.rhatest
+*/
+//	onBeforeRendering: function() {
+//
+//	},
+
+/**
+* Called when the View has been rendered (so its HTML is part of the document). Post-rendering manipulations of the HTML could be done here.
+* This hook is the same one that SAPUI5 controls get after being rendered.
+* @memberOf rhatest.rhatest
+*/
+//	onAfterRendering: function() {
+//
+//	},
+
+/**
+* Called when the Controller is destroyed. Use this one to free resources and finalize activities.
+* @memberOf rhatest.rhatest
+*/
+//	onExit: function() {
+//
+//	}
+
+});
