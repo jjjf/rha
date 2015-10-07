24980fd5ab2ffe4fcf8445f883727a63bf111608
jeff sovetts <jeff.sovetts@sap.com>
2015-10-07 11:54:41 -0400

    Working on getting login

--- rhatest/WebContent/controller/Nav.controller.js
+++ rhatest/WebContent/controller/Nav.controller.js
@@ -1,18 +1,32 @@
 sap.ui.define([
-    'sap/ui/core/mvc/Controller'
-], function(Controller) {
+    'sap/ui/core/mvc/Controller',
+    "sap/m/MessageToast"
+], function (Controller, MessageToast) {
 	"use strict";
 	return Controller.extend("sap.ui.rha.controller.Nav", {
-
 		handleNav: function(evt) {
 			var navCon = this.getView().byId("navCon");
 			var target = evt.getSource().data("target");
 			if (target) {
-				var animation = this.getView().byId("animationSelect").getSelectedKey();
-				navCon.to(this.getView().byId(target), animation);
+				navCon.to(this.getView().byId(target), "slide");
 			} else {
 				navCon.back();
 			}
-		}
+		},
+		doLogin : function (evt) {
+	    	 var u = this.byId('loginUsername').getValue();
+	    	 var p = this.byId('loginPassword').getValue();
+	         var oBundle = this.getView().getModel("i18n").getResourceBundle();
+	    	 if (false) {//(u.length == 0 || p.length == 0)) {
+	    		 var sMsg = oBundle.getText("loginFailed");
+		         MessageToast.show(sMsg);
+	    	 }
+	    	 else {
+	    		 var sMsg = oBundle.getText("loginSucces");
+		         MessageToast.show(sMsg);
+				 var navCon = this.getView().byId("navCon");
+		         navCon.to(this.getView().byId("MyHorses"), "slide");
+	    	 }
+	      }
 	});
 });
